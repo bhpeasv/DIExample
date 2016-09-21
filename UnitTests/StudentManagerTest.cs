aa75cfbfa198894de425d35dafee63bfd4bfa006
Bent H. Pedersen <bhp@easv.dk>
2016-09-22 01:46:37 +0200

    Added throw exception for add existing student

--- UnitTests/StudentManagerTest.cs
+++ UnitTests/StudentManagerTest.cs
@@ -21,6 +21,10 @@ namespace UnitTests
             mock.SetupGet(x => x.Count).Returns(() => students.Count);
             mock.Setup(x => x.Add(It.IsAny<Student>())).Callback<Student>((s) =>
             {
+                if (students.Contains(s))
+                {
+                    throw new ArgumentException("Student already exist");
+                }
                 students.Add(s);
             });
             mock.Setup(x => x.GetById(It.IsAny<int>())).Returns((int id) => students.FirstOrDefault(x => x.Id == id));
@@ -47,6 +51,30 @@ namespace UnitTests
 
             Assert.AreEqual(1, sm.Count);
             Assert.AreSame(student, sm.GetById(student.Id));
+        }
+
+        [TestMethod]
+        public void Add_Existing_Student_Expect_ArgumentException_Test()
+        {
+            IRepository<Student> repository = mock.Object; //new Repository<Student>();
+            StudentManager sm = new StudentManager(repository);
+            Student student = new Student(1, "Name", "Email");
+
+            sm.AddStudent(student);
+
+            Assert.AreEqual(1, sm.Count);
+            Assert.AreSame(student, sm.GetById(student.Id));
+
+            try
+            {
+                sm.AddStudent(student); // try to add the same student again
+                Assert.Fail("Added existing student to repository");
+            }
+            catch (ArgumentException)
+            {
+                Assert.AreEqual(1, sm.Count);
+                Assert.AreSame(student, sm.GetById(student.Id));
+            }
         }
     }
 }
